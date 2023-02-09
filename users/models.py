ffe10804d6d636e8d4a5de3036f2fe2756ffc327
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-02-09 14:56:40 +0200

    Revert "додав forms.py"
    This reverts commit bf32237497d5068a9772dda38841d2f2b1e22e95.

--- users/models.py
+++ users/models.py
@@ -6,12 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserModel(AbstractUser):
-
-    class Meta:
-        db_table = "user"
-        verbose_name = "user"
-        verbose_name_plural = "users"
-        ordering = ["username"]
-
-    def __str__(self):
-        return self.username
+    pass
--- users/views.py
+++ users/views.py
@@ -1,34 +1,58 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 
 from users.models import UserModel
-from users.forms import SignUpForm
 
+
+# def signin_view(request: HttpRequest) -> HttpResponse:
+#     return render(request, "registration/signin.html")
 
 def signin_view(request: HttpRequest):
 
     if request.method == "POST":
-        form = SignInForm(request.POST)
-        if form.is_valid():
-            form.save()
-            redirect_url = reverse_lazy("signin")
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            redirect_url = reverse_lazy("list")
+
             return HttpResponseRedirect(redirect_url)
-    else:
-        form = SignInForm()
 
-    return render(request, "registration/signin.html", {"form": form})
+        return render(request, "registration/signin.html")
 
+    return render(request, "registration/signin.html")
+
+
+# def signup_view(request: HttpRequest) -> HttpResponse:
+#     return render(request, "registration/signup.html")
 
 def signup_view(request: HttpRequest):
 
     if request.method == "POST":
-        form = SignUpForm(request.POST)
-    else:
-        form = SignUpForm()
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
 
-    return render(request, "registration/signup.html", {"form": form})
+        if password != confirm_password:
+            return render(request, "registration/signup.html")
+
+        try:
+            UserModel.objects.get(username=username)
+            return render(request, "registration/signup.html")
+        except UserModel.DoesNotExist:
+            UserModel.objects.create_user(username=username, email=email, password=password)
+
+        redirect_url = reverse_lazy("signin")
+
+        return HttpResponseRedirect(redirect_url)
+
+    return render(request, "registration/signup.html")
 
 
 def logout_view(request: HttpRequest):
