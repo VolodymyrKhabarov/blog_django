a83a26bf1022f9ee61896d8a0f9093b1e55747ee
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-02-23 16:01:49 +0200

    створив серіалайзери для трьох моделей

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,3 +2,29 @@
 Posts application serializers.
 
 """
+
+from rest_framework import serializers
+
+from posts.models import BlogpostCategoryModel, BlogpostModel
+
+
+class BlogpostCategoryModelSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the BlogpostCategoryModel model
+    """
+
+    class Meta:
+        model = BlogpostCategoryModel
+        fields = "id", "name"
+
+
+class BlogpostModelSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the BlogpostModel model
+    """
+
+    category = BlogpostCategoryModelSerializer()
+
+    class Meta:
+        model = BlogpostModel
+        fields = "id", "title", "body", "slug", "created_at", "updated_at", "category"
--- users/serializers.py
+++ users/serializers.py
@@ -2,3 +2,25 @@
 Users application serializers.
 
 """
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+UserModel = get_user_model()
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the UserModel model
+    """
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+
+        model = UserModel
+        fields = "id", "username", "email", "password"
+
+        def create(self, validated_data):
+            user = UserModel.objects.create_user(**validated_data)
+            return user
