360bc17dda472dd33cfa5d163e9bd018694bef9d
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-02-26 10:50:13 +0200

    виправив логіку методу create у моделі BlogpostModelSerializer: якщо у бд немає категорії блогпоста, то виникає помилка 400; якщо категорія блогпоста існує, то після цього можна зберегти блогпост

--- posts/serializers.py
+++ posts/serializers.py
@@ -22,9 +22,17 @@ class BlogpostModelSerializer(serializers.ModelSerializer):
     """
     Serializer for the BlogpostModel model
     """
-
     category = BlogpostCategoryModelSerializer()
 
     class Meta:
         model = BlogpostModel
         fields = "id", "title", "body", "slug", "created_at", "updated_at", "category"
+
+    def create(self, validated_data):
+        category_data = validated_data.pop("category")
+        try:
+            category = BlogpostCategoryModel.objects.get(name=category_data["name"])
+        except BlogpostCategoryModel.DoesNotExist:
+            raise serializers.ValidationError("Category does not exist")
+        blogpost = BlogpostModel.objects.create(category=category, **validated_data)
+        return blogpost
