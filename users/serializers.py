a74f34fe40881150595667708ee2691b37e4042d
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-02-27 22:34:43 +0200

    у UserModelSerializer у метод create додав функцію make_password для хешування паролю

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,7 @@ Users application serializers.
 """
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 UserModel = get_user_model()
@@ -19,3 +20,8 @@ class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = "id", "username", "email", "password"
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        user = super().create(validated_data)
+        return user
