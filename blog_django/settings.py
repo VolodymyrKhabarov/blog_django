68e1c6d8dfcd649706107066bf7218648f468c8a
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-02-28 21:34:03 +0200

    додав 'rest_framework.authtoken', 'rest_framework.authentication.TokenAuthentication', провів міграції

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
     'api',
     'posts',
@@ -99,5 +100,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
