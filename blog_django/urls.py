8cb0baff6f3b4b2fe78e8d15ffb8b1a50714039d
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-03-01 10:25:17 +0200

    переніс path('api/auth/', views.obtain_auth_token, name='api-auth' з blog_django/urls.py у api/urls.py

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -2,11 +2,9 @@
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', views.obtain_auth_token, name='api-auth'),
     path('', include('users.urls')),
     path('', include('posts.urls')),
     path('', include('api.urls')),
