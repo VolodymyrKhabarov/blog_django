cadd1470177fe9e4067e6ee4782df7d27749674a
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-03-01 22:29:58 +0200

    додав docstring

--- api/signals.py
+++ api/signals.py
@@ -1,3 +1,8 @@
+"""
+This module defines the signals used in the API application.
+
+"""
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
@@ -5,7 +10,12 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """
+    Create an authentication token for the user instance after the instance is saved.
+    This function is called by the `post_save` signal sent by Django's User model.
+    """
     if created:
         Token.objects.create(user=instance)
